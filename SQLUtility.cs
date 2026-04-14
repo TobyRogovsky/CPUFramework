e11238ff39019caf02d2c072fa6a7983569bc541
tobyrogovsky <tobyrogovsky@gmail.com>
2026-04-14 18:55:32 -0400

    initial commit

---
+++ SQLUtility.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace CPUFramework
+{
+    public class SQLUtility
+    {
+        public static string ConnectionString = "";
+        public static DataTable GetDataTable(string sqlstatement)
+        {
+            DataTable dt = new DataTable();
+            SqlConnection conn = new();
+            conn.ConnectionString = ConnectionString;
+            conn.Open();
+
+            var cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = sqlstatement;
+            var dr = cmd.ExecuteReader();
+            dt.Load(dr);
+            return dt;
+        }
+    }
+}
