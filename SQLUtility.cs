6e7d02797f61d6668e5d8da484888d81e801cfaa
tobyrogovsky <tobyrogovsky@gmail.com>
2026-06-18 18:50:50 -0400

    tab order shortcut keys tooltips message for null not allowed

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -33,10 +33,11 @@ namespace CPUFramework
         }
 
         private static string ParseConstraintMsg(string msg)
-        {
+        {                      
             string origmsg = msg;
             string prefix = "ck_";
             string msgend = "";
+            string notnullprefix = "Cannot insert the value NULL into column '";
             if (msg.Contains(prefix) == false)
             {
                 if (msg.Contains("u_"))
@@ -47,6 +48,11 @@ namespace CPUFramework
                 else if (msg.Contains("f_"))
                 {
                     prefix = "f_";
+                }
+                else if (msg.Contains(notnullprefix))
+                {
+                    prefix = notnullprefix;
+                    msgend = " cannot be blank";
                 }
             }
             if (msg.Contains(prefix))
